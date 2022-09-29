b5ad9bb10a899ef90b820bfd1c4f7e984a035364
kabradshaw1 <itllbok82@gmail.com>
2022-09-29 15:24:52 -0400

    surveys complete

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 // THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
 // WHEN I enter the team manager’s name, employee ID, email address, and office number
 // THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
-// WHEN I select the engineer option
-// THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
+
 // WHEN I select the intern option
 // THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
 // WHEN I decide to finish building my team
@@ -13,7 +12,7 @@ const generateHtml = require('./scr/html-template');
 const Engineer = require('./lib/Engineer');
 const Manager = require('./lib/Manager');
 const Intern = require('./lib/Intern')
-// let Employees =[]
+
 
 const writeFile = fileContent => {
   return new Promise((resolve, reject) => {
@@ -35,7 +34,6 @@ const writeFile = fileContent => {
 };
 // THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
 const createManager = () => {
-  // Employees = []
   return inquirer.prompt([
     {
       type: 'input',
@@ -54,14 +52,74 @@ const createManager = () => {
     },
     {
       type: 'input',
-      name: 'phone',
-      message: 'What is the manager\'s phone number',
+      name: 'officeNumber',
+      message: 'What is the manager\'s office number',
     },
   ])
   .then(data => {
     return Manager(data)
     // Employees.push(Manager(data));
   })  
+}
+// WHEN I select the engineer option
+// THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
+const promptEngineer = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is the managers name? ',
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: 'What is the manager\'s employee ID? ',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is the manager\'s email address',
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'What is the engineers\'s gethub',
+    },
+  ])
+  .then(data => {
+    return Engineer(data)
+  })
+}
+
+const promptIntern = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is the managers name? ',
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: 'What is the manager\'s employee ID? ',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is the manager\'s email address',
+    },
+    {
+      type: 'input',
+      name: 'school',
+      message: 'What is the intern\'s school',
+    },
+  ])
+  .then(data => {
+    return Intern(data)
+  }
+
+  )
+
 }
 
 const promptNext = () => {
